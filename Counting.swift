1e17fc175783508c9844d6f219448022507ae697
Adrian McDaniel <adriandmcdaniel@gmail.com>
2016-11-29 15:29:02 -0500

    Initial commit

---
+++ Counting.swift
@@ -0,0 +1,44 @@
+//
+//  Counting.swift
+//  
+//
+//  Created by Adrian McDaniel on 11/29/16.
+//
+//
+
+import Foundation
+
+
+let decimalThreeTwoOne = 321
+let binaryThreeTwoOne = 17
+let octalThreeTwoOne = 209
+let hexadecimalThreeTwoOne = 801
+
+let decimalOneThreeFourTwo = 1342
+let binaryOneThreeFourTwo = 29
+let octalOneThreeFourTwo = 738
+let hexadecmialOneThreeFourTwo = 4930
+
+let decimalSixSixThreeTwoFourOne = 663241
+let binarySixSixThreeTwoFourOne = 329
+let octalSixSixThreeTwoFourOne = 222881
+let hexadecimalSixSixThreeTwoFourOne = 6697537
+
+let hexadecimalOneFourAThreeOne = oneFourAThreeOne
+let binaryOneFourAThreeOne = 10011
+let decimalOneFourAThreeOne = 14031
+
+let hexadecimalFFFF = FFFF
+let binaryFFFF = 1111
+let decimalFFFF = 5555
+
+
+
+
+
+
+
+
+
+
+
